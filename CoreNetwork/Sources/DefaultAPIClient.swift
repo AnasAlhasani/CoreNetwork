df8ee05c2810544ddf21b9a213ab3d60981235d1
Anas Alhasani <32222182+AnasAlhasani@users.noreply.github.com>
2019-05-24 00:32:17 +0300

    Fixed response handler (#15)

--- CoreNetwork/Sources/DefaultAPIClient.swift
+++ CoreNetwork/Sources/DefaultAPIClient.swift
@@ -18,7 +18,6 @@ public final class DefaultAPIClient: APIClient {
    }
    
    public func execute<T: APIRequest>(_ request: T) -> Promise<T.Response> {
-      
       let urlRequest = request.urlRequest(in: self)
       let dataRequest = AlamofireManager.default.request(urlRequest)
       AlamofireManager.handleAuthChallenge()
@@ -27,15 +26,15 @@ public final class DefaultAPIClient: APIClient {
       return Promise<T.Response>(on: .global(qos: .background)) { (fullfill, reject) in
          dataRequest.validate().responseJSON { response in
             NetworkLogger.log(data: response.data, response: response.response)
-            if let error = response.error {
-               reject(NetworkError.error(error.localizedDescription))
-            } else if let data = response.data {
+            if let data = response.data {
                do {
                   let object = try JSONDecoder().decode(T.Response.self, from: data)
                   fullfill(object)
                } catch {
                   reject(NetworkError.decodingFailed)
                }
+            } else if let error = response.error {
+               reject(NetworkError.error(error.localizedDescription))
             } else {
                reject(NetworkError.unknown)
             }
